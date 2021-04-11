63247b6db33177645fbceb3ea889d63b3cfc6906
Höppner, Paul <paul.hoeppner@haw-hamburg.de>
2021-04-11 12:20:42 +0200

    changed readme

--- config.h
+++ config.h
@@ -194,9 +194,8 @@ static unsigned int height = 364;
 /*
  * Default colour and shape of the mouse cursor
  */
-static unsigned int mouseshape = XC_xterm;
-static unsigned int mousefg = 7;
-static unsigned int mousebg = 0;
+
+static char* mouseshape = "xterm";
 
 /*
  * Color used to display font attributes when fontconfig selected a font which
